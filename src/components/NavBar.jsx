d7f0ed2a8cb7e0613454a78263210a8137eecc74
richardzleung <richardzleung@gmail.com>
2021-03-16 01:53:05 -0700

    abstracted navbar for readers

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,24 +3,28 @@ import { Link } from 'react-router-dom';
 
 import '../css/Navbar.css';
 
+const Span = props => (
+  <span {...props} aria-hidden='true'/>
+);
+
 const Navbar = () => (
     <div>
       <nav className='nav'>
         <ul id='container'>
           <li>
-            <Link className='nav_tab' to='/'><span data-content='Home' aria-hidden='true'></span>Home</Link>
+            <Link className='nav_tab' to='/'><Span data-content='Home' />Home</Link>
           </li>
           <li>
-            <Link className='nav_tab' to='/about'><span data-content='About' aria-hidden='true'></span>About</Link>
+            <Link className='nav_tab' to='/about'><Span data-content='About'/>About</Link>
           </li>
           <li>
-            <Link className='nav_tab' to='/skills'><span data-content='Skills' aria-hidden='true'></span>Skills</Link>
+            <Link className='nav_tab' to='/skills'><Span data-content='Skills'/>Skills</Link>
           </li>
           <li>
-            <Link className='nav_tab' to='/projects'><span data-content='Projects' aria-hidden='true'></span>Projects</Link>
+            <Link className='nav_tab' to='/projects'><Span data-content='Projects'/>Projects</Link>
           </li>
           <li>
-            <a className='nav_tab' href='#contact'><span data-content='Contact' aria-hidden='true'></span>Contact</a>
+            <a className='nav_tab' href='#contact'><Span data-content='Contact'/>Contact</a>
           </li>
         </ul>
       </nav>
