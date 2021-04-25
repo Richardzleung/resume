7628a723d49006ccb8e85303ae6ba35b6b54a04f
Richard Leung <61563481+Richardzleung@users.noreply.github.com>
2021-04-24 22:11:40 -0700

    added landing page animations

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,13 +1,11 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import HomePageHero from 'styles/landingPage.module.css';
 import scribble from 'static/images/3.svg';
-import { CSSTransition } from 'react-transition-group';
 
 // TODO get this animation working
-const HomeBase = ({ scrollToProjectsView }) => {
-  const scrollbtnRef = useRef();
-  return (
+const HomeBase = ({ scrollToProjectsView }) => (
+  <>
     <section className='vh-100 grid--col' >
       <div className={HomePageHero.background}/>
       <img src={scribble} className={HomePageHero.scribble} alt='scribble-img'/>
@@ -15,21 +13,12 @@ const HomeBase = ({ scrollToProjectsView }) => {
         <span className={HomePageHero.heroTitle}>Front-end Developer</span>
         <span className={HomePageHero.heroSubTitle}>Hello! My name is Richard and I&apos;m a full-stack web developer.</span>
       </h1>
-      <CSSTransition in={true} timeout={400} nodeRef={scrollbtnRef} classNames={{
-        appear: HomePageHero.btnAppear,
-        appearActive: HomePageHero.btnAppearActive,
-        appearDone: HomePageHero.btnAppearDone,
-        enter: HomePageHero.btnAppear,
-        enterActive: HomePageHero.btnAppearActive,
-        enterDone: HomePageHero.btnAppearDone
-      }} appear={true}>
-        <div ref={scrollbtnRef}>
-          <button className='scroll-down' aria-label='scroll-down-btn' onClick={scrollToProjectsView}/>
-        </div>
-      </CSSTransition>
     </section>
-  );
-};
+    <div className='scroll-down--container'>
+      <button className='scroll-down' aria-label='scroll down' onClick={scrollToProjectsView}/>
+    </div>
+  </>
+);
 
 HomeBase.propTypes = {
   scrollToProjectsView: PropTypes.func
