cee38eaa835a52f3d9b18888655cd9c39021f225
Richard Leung <61563481+Richardzleung@users.noreply.github.com>
2021-04-27 00:40:10 -0700

    updated blog nav

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import HomePageHero from 'styles/landingPage.module.css';
+import theme from 'styles/landingPage.module.css';
 import scribble from 'static/images/3.svg';
 import scribble2 from 'static/images/82.svg'
 
@@ -8,23 +8,24 @@ import scribble2 from 'static/images/82.svg'
 const HomeBase = ({ scrollToProjectsView }) => (
   <>
     <section className='vh-100 grid--col' >
-      <span className={HomePageHero.background}/>
-      <img src={scribble} className={HomePageHero.scribble} alt='scribble'/>
-      <img src={scribble2} className={HomePageHero.scribble2} alt='scribble 2'/>
-      <h1 className={HomePageHero.titleContainer}>
-        <span className={HomePageHero.name}>Richard Leung</span>
-        <span className={HomePageHero.message}>Front-End Engineer in San Francisco.</span>
+      <span className={theme.background}/>
+      <img src={scribble} className={theme.scribble} alt='scribble'/>
+      <img src={scribble2} className={theme.scribble2} alt='scribble 2'/>
+      <h1 className={theme.titleContainer}>
+        <span className={theme.name}>Richard Leung</span>
+        <span className={theme.message}>Front-End Engineer in San Francisco.</span>
       </h1>
       <div className='scroll-down--container'>
         <button className='scroll-down' aria-label='scroll down' onClick={scrollToProjectsView}/>
       </div>
     </section>
     <section className='grid--col'>
-      <span className={HomePageHero.about}>
+      <span className={theme.about}>
         Hi there! My name is Richard and I&apos;m temporarily unemployed. 
         That means I&apos;m looking for a job! I started writing a{' '}
-        <a href="https://richardzleung.wordpress.com">blog</a>{' '}
-        under the name Wacko Bandit. I can be found searching for tacos.
+        <a href="https://richardzleung.wordpress.com" className={theme.blog}>
+          blog
+        </a>{' '} under the name Wacko Bandit. I can be found searching for tacos.
       </span>
     </section>
   </>
