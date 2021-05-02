f15dc539597c24693d1313ba77c0ef5705038e5d
Richard Leung <richardzleung@gmail.com>
2021-05-02 14:45:35 -0700

    start of a blog

---
+++ models/blog.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const url = process.env.DB_URL;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {    
+    console.log('connected to MongoDB')  
+  })  
+  .catch((error) => {    
+    console.log('error connecting to MongoDB:', error.message)  
+  })
+
+const blogSchema = new mongoose.Schema({
+  content: String,
+  date: { type: Date, default: Date.now },
+  tags: [String],
+  title: String,
+})
+blogSchema.set('toJSON', {
+  transform: (_, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
