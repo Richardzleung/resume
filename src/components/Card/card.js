af184c909e3f4d5dd1e2c6ba3b32be069b80a79e
Richard Leung <61563481+Richardzleung@users.noreply.github.com>
2021-05-04 17:17:35 -0700

    card fixes

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -8,21 +8,30 @@ const Container = styled.div`
     0 1px 3px rgba(0,0,0,0.10), 
     0 1px 2px rgba(0,0,0,0.30);  
   max-width: 45ch;
-  padding: 10rem 0;
-  overflow: hidden;
+  min-height: 45ch;
+  padding: 10em 0;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  /* overflow: hidden; */
 `;
 
 const Content = styled.div`
   background: linear-gradient(
-    hsl(0 0% 0% / 1),
-    hsl(0 0% 0% / 0)
+    hsl(0 0% 0% / .75),
+    hsl(255 50% 50% / 1) 10%,
+    hsl(0 0% 0% / 1)
   );
+  line-height: 1.6;
   padding: 1.5em;
+  position: absolute;
+  bottom:0;
 `;
 
 const Title = styled.div`
   position: relative;
   width: max-content;
+  
 
   &::after {
     background: red;
@@ -38,12 +47,14 @@ const Title = styled.div`
 const Button = styled.a`
   cursor: pointer;
   border-radius: .25em;
-  display: inline-block;
+  display: block;
   text-decoration: none;
   color: black;
   background-color: white;
+  margin-bottom: .5em;
   padding: 0.5em 1.25em;
-
+  position: relative;
+  top: 1rem;
   &:hover,
   &:focus {
     background-color: pink;
